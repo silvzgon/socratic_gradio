6060d23a291e78421cc91308346dc66b4ad556cb
silvzgon <141477869+silvzgon@users.noreply.github.com>
2024-10-24 23:55:57 +0800

    Update test_gradio.py

--- socratic_gradio/test_gradio.py
+++ socratic_gradio/test_gradio.py
@@ -1,9 +1,11 @@
 import gradio as gr
 from openai import OpenAI
+import os
+import uuid
 import json
 import prompts
 
-key = '' # api key here
+key = os.getenv('OPENAI_API_KEY') # api key here
 max_try = 3
 client = OpenAI(api_key=key)
 
@@ -14,146 +16,137 @@ def agent_calling(messages):
         try:
             completion = client.chat.completions.create(
                 model='gpt-4o',
-                messages=messages
+                messages=messages,
+                stream=True
             )
-            return completion.choices[0].message.content
+            response = ""
+            for chunk in completion:
+                chunk_message = getattr(chunk.choices[0].delta, 'content', None)
+                if chunk_message:
+                    response += chunk_message
+                yield response
+            return
         except Exception as e:
             if count > max_try:
                 raise Exception(f'调用AI代理时出错。错误：{e}')
             else:
                 print('[警告] 调用AI代理时出错，正在重试...')
 
-def load_history():
-    try:
-        with open('history/history.json', 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except Exception as e:
-        print(f'[警告] 读取历史记录时出错，自动初始化。错误: {e}')
-        return []
-
-def save_history(history):
-    with open('history/history.json', 'w', encoding='utf-8') as file:
-        json.dump(history, file, ensure_ascii=False, indent=4)
-
 def load_steps():
     with open('temp/step.json', 'r', encoding='utf-8') as file:
         return json.load(file)
 
-def chat_interface(message):
-    global current_step, steps, messages, chat_history
+def get_file(chat_history):
+    filename = 'history/' + str(uuid.uuid4()) + '.json'
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(chat_history, file, ensure_ascii=False, indent=4)
+    return filename
+
+def chat_interface(message, current_step, messages, chat_history):
+    global steps
 
     if not messages:
-
         # 开始新的步骤
         if current_step < len(steps):
             step = steps[current_step]
-            response1 = agent_calling([
+            gen = agent_calling([
                 {'role': 'system', 'content': prompts.subtasks_start_system},
                 {'role': 'user', 'content': prompts.subtasks_start + step}
             ])
+            for response1 in gen:
+                yield [response1], current_step, messages, chat_history
             chat_history.append(f"【老师】：{response1}")
             messages = [
                 {'role': 'system', 'content': prompts.subtasks_system},
                 {'role': 'user', 'content': prompts.subtasks + steps[current_step] + '\n#输出：'}
             ]
-            response2 = agent_calling(messages)
+            gen = agent_calling(messages)
+            for response2 in gen:
+                yield [response1, response2], current_step, messages, chat_history
             messages.append({'role': 'assistant', 'content': response2})
             chat_history.append(f"【老师】：{response2}")
-            save_history(chat_history)
-            return [response1, response2]
         
-        # 全部步骤结束，程序自动保存
+        # 全部步骤结束
         else:
             current_step = 0
-            save_history(chat_history)
-            return ["所有步骤已完成。是否要重新开始？"]
-    
-    # 跳过当前步骤
-    if message.lower() == '\\skip':
-        current_step += 1
-        if current_step < len(steps): return ["好的，我们跳过这个步骤。"]
-        else:
-            current_step = 0
-            messages = []
-            return ["好的，我们跳过这个步骤。", "所有步骤已完成。是否要重新开始？"]
+            yield ["所有步骤已完成。是否要重新开始？"], current_step, messages, chat_history
+
+        return
     
     else:
         messages.append({'role': 'user', 'content': message})
         chat_history.append(f"【学生】：{message}")
-        response1 = agent_calling(messages)
-        messages.append({'role': 'assistant', 'content': response1})
-        chat_history.append(f"【老师】：{response1}")
-        
-        # 添加步骤结束后的完整代码检查
-        if '【结束】' in response1:
-            response1 = response1.replace('【结束】', '')
-        #     messages = [
-        #         {'role': 'system', 'content': prompts.subtasks_end_system},
-        #         {'role': 'user', 'content': prompts.subtasks_end + steps[current_step]}
-        #     ]
-        #     response2 = agent_calling(messages)
-        #     messages.append({'role': 'assistant', 'content': response2})
-        #     chat_history.append(f"【老师】：{response2}")
-        #     current_step += 1
-        #     return [response1, response2]
-
-        # # 当前步骤结束，进行下一步骤
-        # elif '【正确】' in response1:
-        #     response1 = response1.replace('【正确】', '')
+        gen = agent_calling(messages)
+        flag = False
+        for response in gen:
+            if '【结束】' in response:
+                flag = True
+            else:
+                yield [response], current_step, messages, chat_history
+        messages.append({'role': 'assistant', 'content': response})
+        chat_history.append(f"【老师】：{response}")
+        if flag:
+            print(response)
+            response = response.replace('【结束】', '')
+            yield [response], current_step, messages, chat_history
             current_step += 1
             messages = []
-            response = chat_interface("")
-            response.insert(0, response1)
-            return response
+            gen = chat_interface("", current_step, messages, chat_history)
+            for result in gen:
+                n_response, current_step, messages, chat_history = result
+                n_response.insert(0, response)
+                yield n_response, current_step, messages, chat_history
 
-        return [response1]
+def user(user_message, history):
+    return "", history + [[user_message, None]]
 
-# 全局变量
-current_step = 0
-steps = []
-messages = []
-chat_history = []
+def bot(history, current_step, messages, chat_history):
+    user_message = history[-1][0]
+    gen = chat_interface(user_message, current_step, messages, chat_history)
+    for result in gen:
+        bot_message, step, messages, chat_history = result
+        if not history[-1][0] and step != current_step:
+            history = history[:-(len(bot_message)-1)]
+            current_step = step
+        elif not history[-1][0]:
+            history = history[:-len(bot_message)]
+        for i in range(len(bot_message)):
+            history.append([None, bot_message[i]])
+        yield history, current_step, messages, chat_history
+
+def reset():
+    current_step = 0
+    messages = []
+    chat_history = []
+    gen = chat_interface("", current_step, messages, chat_history)
+    for result in gen:
+        history = []
+        bot_message, current_step, messages, chat_history = result
+        for i in range(len(bot_message)):
+            history.append([None, bot_message[i]])
+        yield history, current_step, messages, chat_history
+
+steps = load_steps()
 
 # 创建Gradio界面
 with gr.Blocks() as iface:
     
     # 初始化
-    current_step = 0
-    steps = load_steps()
-    chat_history = load_history()
+    current_step = gr.State(value=0)
+    messages = gr.State(value=[])
+    chat_history = gr.State(value=[])
 
-    bot_message = chat_interface("")
-    history = [[None, bot_message[0]], [None, bot_message[1]]]
-
-    chatbot = gr.Chatbot(label="对话历史", value=history)
+    chatbot = gr.Chatbot(label="对话历史", value=[])
     msg = gr.Textbox(label="输入")
-    download = gr.DownloadButton(label="下载记录", value='history/history.json')
-    clear = gr.Button("清除对话")
+    clear = gr.Button("开始对话 / 重新开始")
+    download = gr.Button("下载记录")
+    file_output = gr.File(label="Download", interactive=False)
 
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
-
-    def bot(history):
-        user_message = history[-1][0]
-        bot_message = chat_interface(user_message)
-        save_history(chat_history)
-        for i in range(len(bot_message)):
-            history.append([None, bot_message[i]])
-        return history, gr.update(value='history/history.json')
-    
-    def reset():
-        global current_step, messages, chat_history
-        current_step = 0
-        messages = []
-        chat_history = []
-        bot_message = chat_interface("")
-        history = [[None, bot_message[0]], [None, bot_message[1]]]
-        save_history(chat_history)
-        return history, gr.update(value='history/history.json')
-
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, [chatbot, download])
-    clear.click(reset, None, [chatbot, download], queue=False)
+    msg.submit(user, [msg, chatbot], [msg, chatbot]).then(bot, [chatbot, current_step, messages, chat_history], [chatbot, current_step, messages, chat_history])
+    clear.click(reset, None, [chatbot, current_step, messages, chat_history])
+    download.click(get_file, chat_history, file_output)
 
 # 运行Gradio应用
 if __name__ == "__main__":
-    iface.launch()
+    iface.queue()
+    iface.launch(share=True)
